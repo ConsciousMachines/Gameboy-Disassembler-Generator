5336be4f1f334a74a920af2ddc4e56c7ca66ed31
ConsciousMachines <pwnagecorp2@gmail.com>
2021-10-10 17:20:49 +0500

    Update gb_disassembler_generator.py

--- gb_disassembler_generator.py
+++ gb_disassembler_generator.py
@@ -5,13 +5,17 @@ all_of_it = all_of_it.replace('\n', ' ')
 opcodes = all_of_it.split('class="opcode')[1:] 
 
 
+cat2int = {'control misc' : 0, 'control br' : 1, 'x16 lsm' : 2, 'x8 alu' : 3, 'x8 rsb' : 4, 'x8 lsm' : 5, 'unused' : 6, 'x16 alu' : 7}
+
 # THIS SECTION OF CODE IS FOR INSTR 0-255
 for i in range(len(opcodes[:256])):
     hex_idx = hex(i)[2:].upper().rjust(2,"0")
     entry = opcodes[i]
+    category = entry.split('"')[0].strip()
+    cat_int = cat2int[category]
 
     if entry.__contains__('unused'): # for the unused opcodes 
-        full_instr = f'case 0x{hex_idx}: return dis_line{{ pc, 1, {{b1,b2,b3}}, "UNUSED\\n" }};'
+        full_instr = f'case 0x{hex_idx}: return dis_line{{ pc, 1, {{b1,b2,b3}}, "UNUSED\\n", {cat_int} }};'
         print(full_instr)
         continue
 
@@ -23,21 +27,21 @@ for i in range(len(opcodes[:256])):
     instr = ' '.join(parts[:-2])
     
     if int(size) == 1:
-        full_instr = f'case 0x{hex_idx}: return dis_line{{ pc, {size}, {{b1,b2,b3}},"{instr}\\n" }};'
+        full_instr = f'case 0x{hex_idx}: return dis_line{{ pc, {size}, {{b1,b2,b3}},"{instr}\\n", {cat_int} }};'
 
     elif int(size) == 2:
         if (instr.__contains__('u8')): # immediate unsigned u8
             instr2 = instr.replace('u8', '$%02x') 
-            full_instr = f'case 0x{hex_idx}: {{dis_line l = dis_line{{ pc, {size}, {{b1,b2,b3}},"" }}; sprintf(l._text, "{instr2}\\n", b2); return l;}}'
+            full_instr = f'case 0x{hex_idx}: {{dis_line l = dis_line{{ pc, {size}, {{b1,b2,b3}},"" , {cat_int} }}; sprintf(l._text, "{instr2}\\n", b2); return l;}}'
         elif (instr.__contains__('i8')): #immediate signed i8, JR
             instr2 = instr.replace('i8', '$%02x') 
-            full_instr = f'case 0x{hex_idx}: {{dis_line l = dis_line{{ pc, {size}, {{b1,b2,b3}},"" }}; sprintf(l._text, "{instr2} [%hhi]\\n", b2,b2); return l;}}'
+            full_instr = f'case 0x{hex_idx}: {{dis_line l = dis_line{{ pc, {size}, {{b1,b2,b3}},"" , {cat_int} }}; sprintf(l._text, "{instr2} [%hhi]\\n", b2,b2); return l;}}'
         else: # STOP instruction 
-            full_instr = f'case 0x{hex_idx}: return dis_line{{ pc, {size}, {{b1,b2,b3}},"{instr}\\n" }};'
+            full_instr = f'case 0x{hex_idx}: return dis_line{{ pc, {size}, {{b1,b2,b3}},"{instr}\\n" , {cat_int} }};'
 
     elif int(size) == 3:
         instr2 = instr.replace('u16', '$%02x%02x')
-        full_instr = f'case 0x{hex_idx}: {{dis_line l = dis_line{{ pc, {size}, {{b1,b2,b3}},"" }}; sprintf(l._text, "{instr2}\\n", b3, b2); return l;}}'
+        full_instr = f'case 0x{hex_idx}: {{dis_line l = dis_line{{ pc, {size}, {{b1,b2,b3}},"" , {cat_int} }}; sprintf(l._text, "{instr2}\\n", b3, b2); return l;}}'
 
     print(full_instr)
 
@@ -48,6 +52,8 @@ for i in range(len(opcodes[:256])):
 # CB INSTRUCTIONS
 for i in range(256, len(opcodes)):
     entry = opcodes[i]
+    category = entry.split('"')[0].strip()
+    cat_int = cat2int[category]
     text = entry.split('<pre>')[1].split('</pre>')[0]
     assert text.count('t') <= 2, "more than 2 t's found!"
     parts = text.split('t')[0].split(' ')
@@ -56,5 +62,6 @@ for i in range(256, len(opcodes)):
     instr = ' '.join(parts[:-2])
 
     hex_idx2 = hex(i - 256)[2:].upper().rjust(2,"0")
-    full_instr = f'case 0x{hex_idx2}: return dis_line{{ pc, {size}, {{0xCB,0x{hex_idx2},0x00}},"{instr}\\n" }};'
+    full_instr = f'case 0x{hex_idx2}: return dis_line{{ pc, {size}, {{0xCB,0x{hex_idx2},0x00}},"{instr}\\n" , {cat_int} }};'
     print(full_instr)
+
